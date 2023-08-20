6a1c0a5a24f318763da47cd591a315666239ffa4
Adrian Sudak <adriansudak@outlook.com>
2023-08-20 15:17:04 +0200

    Refactor - simplifying time

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
-import { GameQuery } from "../App";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
+import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
@@ -44,7 +45,7 @@ const useGames = (gameQuery: GameQuery, { pageSize }: QueryPage) => {
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms("24h"),
   });
 };
 
