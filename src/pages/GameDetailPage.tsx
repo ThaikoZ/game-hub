95df7287baa358af739153eecc2cf319cd5b7a7b
Adrian Sudak <adriansudak@outlook.com>
2023-08-22 16:28:15 +0200

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -13,7 +14,19 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{data.name}</Heading>
-      <Text>{data.description_raw}</Text>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quae
+        recusandae nobis officiis similique qui assumenda corrupti,
+        necessitatibus accusantium natus magni earum provident labore magnam
+        laborum dolor aliquam voluptatibus nostrum ut doloremque unde sed?
+        Voluptatem soluta consectetur, harum provident deleniti, nam labore
+        facere aliquam tempore excepturi quos culpa commodi? Enim corporis
+        necessitatibus quasi totam, modi asperiores harum sequi assumenda velit
+        quia commodi labore optio magnam unde! Vero facilis facere quam possimus
+        eligendi qui enim pariatur, temporibus dicta sit aliquam, quod a quidem
+        corrupti nihil perferendis, animi ullam ea. Eligendi ipsam sint, commodi
+        eaque nobis cumque eveniet. Illo sit vero eveniet!
+      </ExpandableText>
     </>
   );
 };
